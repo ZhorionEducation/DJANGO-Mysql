3320607a1327e760a428dbfd5fe475653b6f46e3
Cristian Felipe Buitrago Quintero <cristian_fbuitrago@soy.sena.edu.co>
2026-04-10 06:51:55 -0500

    requirements y allowed host

--- proyecto_tienda/settings.py
+++ proyecto_tienda/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-_zhzp_s+8*z0!ua8dq&0^*h(1mcz(smnd@tgygmkb3e^avt1bh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
